9f9fbfd257b8613f01761b1b9958b1c7a12ef43e
fajar sidik priatna <jardik.oryza@gmail.com>
2024-01-06 20:49:11 +0700

    update link github and readme

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,9 +4,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>filament suitcms plugin docs</span>,
   project: {
-    link: 'https://github.com/jhonoryza/filament-suitcms-plugin-docs',
+    link: 'https://github.com/jhonoryza/filament-suitcms-plugin',
   },
-  docsRepositoryBase: 'https://github.com/jhonoryza/filament-suitcms-plugin-docs',
+  docsRepositoryBase: 'https://github.com/jhonoryza/filament-suitcms-plugin',
   footer: {
     text: 'filament suitcms plugin docs',
   },
