0c3c5a64e8eee447b54f0a364464c1eedf8d3216
fajar sidik priatna <jardik.oryza@gmail.com>
2026-02-01 14:15:20 +0700

    set package ver

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+// next.config.js
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+  themeConfig: './theme.config.tsx', // cukup path, jangan import
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
